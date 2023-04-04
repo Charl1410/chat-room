c000eafc582c7de6852faa989131eb882ff30900
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-04 22:36:15 +0100

    drop down

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,7 +7,8 @@ const Login = () => {
 
   return (
    <div className='container flex justify-center items-center w-full flex-col'>
-       <form className='border-solid border-2 border-indigo-600 flex flex-col p-5'>
+        <h1>Welcome to the Chat App, Enter a username and pick a room to join</h1>
+       <form className='border-solid border-2 border-indigo-600 flex flex-col p-5 justify-center items-center'>
             <input
                 value={userName}
                 name="userName"
@@ -16,16 +17,18 @@ const Login = () => {
 
              />
             
-              <input
-                value={userName}
-                name="userName"
-                type='text'
-                placeholder="username"
+            <div className="relative w-full lg:max-w-sm">
+                <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                    <option>Select Room to Join</option>
+                    <option>Laravel 9 with React</option>
+                    <option>React with Tailwind CSS</option>
+                    <option>React With Headless UI</option>
+                </select>
+            </div>
 
-             />
-
-             <button>Join Room</button>
-
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Join Room
+            </button>
 
        </form>
 

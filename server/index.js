fa8467718b5f35f7ae613e67dcc0e32f736f0c48
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-04-04 22:38:25 +0100

    began to add mongoose

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
+http = require('http');
+const cors = require('cors');
+const path = require('path');
 
+app.use(cors()); // Add cors middleware
+app.use(express.json());

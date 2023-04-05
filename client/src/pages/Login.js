fd5ee085fa081831b51e45a1bbfae108b3ca89d7
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-05 10:12:07 +0100

    added router to appjs for login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -27,9 +27,9 @@ const Login = () => {
             <div className="relative w-full lg:max-w-sm">
                 <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
                     <option>Select Room to Join</option>
-                    <option>Laravel 9 with React</option>
-                    <option>React with Tailwind CSS</option>
-                    <option>React With Headless UI</option>
+                    <option>React</option>
+                    <option>Svelt</option>
+                    <option>Daisy UI</option>
                 </select>
             </div>
 

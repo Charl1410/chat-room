2b2051b1b8fa99b285d57ccdebae14b386cc3c72
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-12 22:30:29 +0100

    created function to pull through the message data and format the date

--- client/src/components/SentMessages.js
+++ client/src/components/SentMessages.js
@@ -6,7 +6,31 @@ import React from 'react'
 function SentMessages() {
   const Messages = ({socket}) => {
     //setting messagesRecieved to an empty array in this state
-    const[messagesRecieved, setMessagesRecieved] = useState([])
+    const[messagesRecieved, setMessagesRecieved] = useState([]);
+
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+          console.log(data);
+          setMessagesReceived((state) => [
+            ...state,
+            {
+              message: data.message,
+              username: data.username,
+              __createdtime__: data.__createdtime__,
+            },
+          ]);
+        });
+    
+        // dunno 
+        return () => socket.off('receive_message');
+      }, [socket]);
+
+    //function to format the date 
+      const formatDateFromTimestamp = (timestamp) => {
+        const date = new Date(timestamp);
+        return date.toLocaleString()
+    }
+
   }
   
     return (

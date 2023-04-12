0d94e9260ca1e108e5b99dd6ecc0afb5073172fb
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-12 22:08:33 +0100

    created page for chat messages, created state to hold messages recieved

---
+++ client/src/components/SentMessages.js
@@ -0,0 +1,19 @@
+import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+import React from 'react'
+
+function SentMessages() {
+  const Messages = ({socket}) => {
+    //setting messagesRecieved to an empty array in this state
+    const[messagesRecieved, setMessagesRecieved] = useState([])
+  }
+  
+    return (
+    <div className="w-96 h-96 border-indigo-500">
+        
+    </div>
+  )
+}
+
+export default SentMessages
